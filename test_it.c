8fc82373aeb6a5f9e5758001a2c0ceb3d3c3d371
Thomas Van Doren <thomas.vandoren@gmail.com>
2015-04-10 01:55:04 -0700

    Fix the chpl_exec.c test.

--- test_it.c
+++ test_it.c
@@ -6,21 +6,22 @@
 
 
 int main() {
+  /*
   const int size = 2 * chpl_get_environ_size();
   const char* env[size];
   chpl_get_environ(env, size);
 
   for (int i = 0; i < size; i++)
     printf("%s\n", env[i]);
+  */
 
-  /*
-  char* output;
+  const char* output;
   int result = chpl_run("ls -l ~/Dropbox", &output);
   //save_cmd("ls -l ~/Dropbox");
   printf("output:\n%s\n", output);
   printf("result = %i\n", result);
   //  printf("output:\n%s\n", output);
   // printf("strlen(output) = %lu\n", strlen(output));
-  */
+
   return 0;
 }
